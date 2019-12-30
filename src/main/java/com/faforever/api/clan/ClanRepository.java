fe5ae19a150a6e942c1bee82f7cec7752a8e09d8
kubo <jsodpad@gmail.com>
2019-12-30 15:03:34 +0100

    @Transactional on clan repository find method

--- src/main/java/com/faforever/api/clan/ClanRepository.java
+++ src/main/java/com/faforever/api/clan/ClanRepository.java
@@ -3,11 +3,14 @@ package com.faforever.api.clan;
 import com.faforever.api.data.domain.Clan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface ClanRepository extends JpaRepository<Clan, Integer> {
+
+  @Transactional
   Optional<Clan> findOneByName(String name);
 
   Optional<Clan> findOneByTag(String tag);
